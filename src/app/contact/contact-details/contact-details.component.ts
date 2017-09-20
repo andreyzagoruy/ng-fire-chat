50f7806f2b2906a8976d04912f0132701167ed34
Andrey Zagoruy <andreyzagoruy@Andreys-MacBook-Air.local>
2017-09-20 20:23:05 +0300

    save to firebase done

--- src/app/contact/contact-details/contact-details.component.ts
+++ src/app/contact/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'contact-details',
@@ -6,7 +7,16 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./contact-details.component.scss']
 })
 export class ContactDetailsComponent {
+  contacts: FirebaseListObservable<any[]>;
+
+  constructor(private db: AngularFireDatabase) {
+      this.contacts = this.db.list('/contacts');
+  }
 
   @Input() contact;
+
+  updateContact() {
+    this.contacts.update(this.contact.$key, this.contact);
+  }
 
 }
